36ee4bf18555a79834d645a59cdc8b32ada9e646
Denis Vovchenko <allergyman@mail.ru>
2026-03-02 14:01:28 +0400

    added coaches and athletes

--- app_run/views.py
+++ app_run/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import viewsets
 from .models import Run
-from .serializers import RunSerializer
+from .serializers import RunSerializer, UserSerializer
 from django.conf import settings
 
 # Create your views here.
@@ -18,3 +19,20 @@ def company_details(request):
 class RunViewSet(viewsets.ModelViewSet):
     queryset = Run.objects.all()
     serializer_class = RunSerializer
+
+
+
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def get_queryset(self):
+        qs = User.objects.filter(is_superuser=False)  # Исключаем суперпользователей
+        type = self.request.query_params.get('type')
+
+        if type == 'coach':  # Если параметр равен 'coach', фильтруем по coach
+            qs = qs.filter(is_staff=True)  # Фильтруем по is_staff=True, если параметр указан
+        elif type == 'athlete':  # Если параметр равен 'athlete', фильтруем по athlete
+            qs = qs.filter(is_staff=False)  # Фильтруем по is_staff=False, если параметр указан
+
+        return qs
